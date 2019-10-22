0ee81047d70e85f4ffa95236cdd78e7d9fe370b6
Jessica Lin <JLin@ucvts.tec.nj.us>
2019-10-22 13:22:43 -0400

    exercise 3

---
+++ src/area.js
@@ -0,0 +1,11 @@
+const readlineSync = require("readline-sync");
+
+const width = readlineSync.question("\nWidth: ");
+const length = readlineSync.question("Length: ");
+
+let nm = 25.4 ;
+
+const widthNM = width * nm ;
+const lengthNM = length * nm ;
+let area = widthNM * lengthNM ;
+console.log("\nA(n) " + width + "-by-" +length + "-inch sheet of paper has an area of " + area + " square millimeter(s).");
