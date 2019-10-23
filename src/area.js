d5184b9620dc69e352c7186183ff1e4911e328bb
Jessica Lin <55418635+woxiangsui@users.noreply.github.com>
2019-10-23 09:49:53 -0400

    Update area.js

--- src/area.js
+++ src/area.js
@@ -8,5 +8,6 @@ let nm = 25.4 ;
 const widthNM = width * nm ;
 const lengthNM = length * nm ;
 let area = widthNM * lengthNM ;
-const area = area.toLocalString("en"); 
+const area = Math.round(100 * area);
+const area = area.toLocalString("en");
 console.log("\nA(n) " + width + "-by-" +length + "-inch sheet of paper has an area of " + area + " square millimeter(s).");
