131c7088288f9559545472a05c7c61493cadd2b9
Jessica Lin <JLin@ucvts.tec.nj.us>
2019-10-24 12:01:30 -0400

    fixing changes

--- src/area.js
+++ src/area.js
@@ -8,12 +8,4 @@ let area = width * length * mm * mm ;
 const areaI = Math.round(100 * area)/100 ;
 const areaF = areaI.toLocaleString("en");
 
-<<<<<<< HEAD
-const widthNM = width * nm ;
-const lengthNM = length * nm ;
-let area = widthNM * lengthNM ;
-area = Math.
-console.log("\nA(n) " + width + "-by-" +length + "-inch sheet of paper has an area of " + area + " square millimeter(s).");
-=======
 console.log("\nA(n) " + width + "-by-" + length + "-inch sheet of paper has an area of " + areaF + " square millimeter(s).");
->>>>>>> 97a4db5d8baaead5852c849e4944716c03504a51
