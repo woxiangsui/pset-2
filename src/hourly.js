c2857f1b0515407ec7bf6037c4755e27ffb760da
unknown <jlin@ucvts.org>
2019-10-24 19:02:22 -0400

    updating ex 7

--- src/hourly.js
+++ src/hourly.js
@@ -9,4 +9,8 @@ const friHour = Number(readlineSync.question("Friday: "));
 const satHour = Number(readlineSync.question("Saturday: "));
 const sunHour = Number(readlineSync.question("Sunday: "));
 
-console.log("\nYou'll make $" + "this week.");
+const wage = 12.5 ;
+const earnings = (monHour * wage) + (tueHour * wage) + (wedHour * wage) + (thuHour * wage) + (friHour * wage) + (satHour * wage) + (sunHour * wage)
+const earnf = earnings.toLocaleString("en", Math.round(100 * earnings)/100);
+
+console.log("\nYou'll make $" + earnf + " this week.");
