a6d54f4292683235d6e0283c7f93cb046cd568bf
unknown <jlin@ucvts.org>
2019-10-23 21:29:41 -0400

    fixed minor mistakes

--- src/area.js
+++ src/area.js
@@ -6,6 +6,6 @@ const length = readlineSync.question("Length: ");
 const mm = 25.4 ;
 let area = width * length * mm * mm ;
 const areaI = Math.round(100 * area)/100 ;
-const areaF = area.toLocaleString("en");
+const areaF = areaI.toLocaleString("en");
 
 console.log("\nA(n) " + width + "-by-" + length + "-inch sheet of paper has an area of " + areaF + " square millimeter(s).");
